2737de2c2a5532a41b556229fdd1b0c7a30f0999
Oleg Tishkin <umkasanki@gmail.com>
2026-09-14 18:39:31 +0300

    [TEAM-612] Add plugin class

---
+++ src/Plugin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @copyright Copyright (c) Your Next Agency
+ */
+
+namespace yna\mailtrap;
+
+/**
+ * Mailtrap plugin.
+ *
+ * @author Your Next Agency <developers@yournextagency.com>
+ * @since 1.0.0
+ */
+class Plugin extends \craft\base\Plugin
+{
+}
